3262718ec5c00cba732e1a4ffbf94e9ed59cf365
bhavjha <112339205+bhavjha@users.noreply.github.com>
2022-09-15 23:39:37 -0400

    working code

--- solution-hw2/js/script.js
+++ solution-hw2/js/script.js
@@ -1,48 +1,46 @@
 
 
 // 6 objects - 1 for each type of roll 
+let rolls = {
+    'originalCinnamonRoll': {
+        rolltype: "Original cinnamon roll",
+        price: 2.49,
+        glazing: "",
+        packSize: 0,
+    },
+    'appleCinnamonRoll' : {
+        rolltype: "Apple cinnamon roll",
+        price: 3.49,
+        glazing: "",
+        packSize: 0,
+    },
+    'raisinCinnamonRoll' : {
+        rolltype: "Raisin cinnamon roll",
+        price: 2.99,
+        glazing: "",
+        packSize: 0,
+    },
+    'walnutCinnamonRoll' : {
+        rolltype: "Walnut cinnamon roll",
+        price: 3.49,
+        glazing: "",
+        packSize: 0,
+    },
+    'doubleChocolateCinnamonRoll' : {
+        rolltype: "Double-chocolate cinnamon roll",
+        price: 3.99,
+        glazing: "",
+        packSize: 0,
+    },
+    'strawberryCinnamonRoll' : {
+        rolltype: "Strawberry cinnamon roll",
+        price: 3.99,
+        glazing: "",
+        packSize: 0,
+    }
 
-let originalCinnamonRoll = {
-    rolltype: "Original cinnamon roll",
-    price: 2.49,
-    glazing: 0,
-    packSize: 0,
-};
+}
 
-let appleCinnamonRoll = {
-    rolltype: "Apple cinnamon roll",
-    price: 3.49,
-    glazing: 0,
-    packSize: 0,
-};
-
-let raisinCinnamonRoll = {
-    rolltype: "Raisin cinnamon roll",
-    price: 2.99,
-    glazing: 0,
-    packSize: 0,
-};
-
-let walnutCinnamonRoll = {
-    rolltype: "Walnut cinnamon roll",
-    price: 3.49,
-    glazing: 0,
-    packSize: 0,
-};
-
-let doubleChocolateCinnamonRoll = {
-    rolltype: "Double-chocolate cinnamon roll",
-    price: 3.99,
-    glazing: 0,
-    packSize: 0,
-};
-
-let strawberryCinnamonRoll = {
-    rolltype: "Strawberry cinnamon roll",
-    price: 3.99,
-    glazing: 0,
-    packSize: 0,
-};
 
 //connecting glazing options and its price adaptation
 const glazingOptions = {
@@ -100,39 +98,65 @@ function glazingOptionsDisplay() {
 glazingOptionsDisplay();
 
 let pr = 0;
+let pr_text = "";
 let pa = 0;
+let no_items = 0;
+let items_total = 0.0;
+
 
 function glazingChange(element) {
     // get price value of selected glazing option
-    const priceChange = element.value; //remove const since using global
     pr = element.value;
+    //https://stackoverflow.com/questions/14976495/get-selected-option-text-with-javascript
+    pr_text = element.options[element.selectedIndex].text;
+    console.log('selected glaze was -', pr_text, pr);
   }
 
 function packSizeChange(element) {
     // get chosen pack size
-    const packChange = element.innerText || element.textContent; //remove const since using global
     pa = element.innerText || element.textContent;
 }
 
- // add your code to do update the price ...
-  //(basePrice + glazingPrice) * packPrice
+ // add your code to do update the price ... (basePrice + glazingPrice) * packPrice
    
-function popUpSummary(){
-    var popup = document.getElementById("popupSummary");
+function popUpSummary(roll,glaze,pack,total){
+    let popup = document.getElementById("popupSummary");
+    popup.innerHTML = "<b>" +roll + "</b>" + "</br>" + glaze + "<br/>" + "Pack of " + pack + "<br/>Price: $" + total;
     popup.classList.toggle("show")
 }
 
+function popUpHide() {
+    let popup = document.getElementById("popupSummary");
+    popup.style.display='none';
+}
+
 function addToCart(idOfRollPrice){
-    let cost = document.getElementById(idOfRollPrice);
+    let cost = document.getElementById(idOfRollPrice); //gets name of roll
     let costValue = cost.innerText.slice(2) || cost.textContent.slice(2);
     // console.log(typeof pr, pr,typeof pa, pa, typeof costValue, costValue);    
     //all strings so convert to Int
     //https://stackoverflow.com/questions/1133770/how-to-convert-a-string-to-an-integer-in-javascript
-    cost.innerHTML = '$ ' + ( parseFloat(costValue) + parseFloat(pr) ) * parseFloat(pa);
+    finalCost = ( parseFloat(costValue) + parseFloat(pr) ) * parseFloat(pa);
+    cost.innerHTML = '$ ' + finalCost;
 
     //add the roll with its configured glazing and packSize to a list that represents the cart items
+    let cart = document.getElementById("cart-summary-items");
+    no_items += 1;
+    cart.innerHTML = no_items + " items";
+    let cartTotal = document.getElementById("cart-summary-items-total"); 
+    items_total += finalCost;
+    cartTotal.innerHTML = "Total: $" + items_total.toFixed(2);
 
     //call Pop up for 3 seconds
+    popUpSummary(rolls[idOfRollPrice].rolltype, pr_text, pa, finalCost);
+    setTimeout(popUpHide,3000);
+
+
+
+
+    pr = 0;
+    pr_text = "";
+    pa = 0;
 }
 
 
